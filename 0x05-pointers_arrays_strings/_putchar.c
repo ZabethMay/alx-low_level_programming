558bfa3e854d9191f82317c9066e009ba0821821
Zabeth Adeniji <zee.adex@gmail.com>
2023-05-02 18:05:54 -0700

    task 0, 0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,10 @@
+#include <unistde.h>
+
+/**
+ * _putchar - writes character c to stdout
+ * @C: char to be printed
+ * Return: On success 1
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
